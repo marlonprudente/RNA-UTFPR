0855b2633e5c0424dc9b3d6526b08da622058fa6
Marlon Prudente <marlonoliveira@alunos.utfpr.edu.br>
2018-04-15 12:17:51 -0300

    Inicio de implementação MLP

---
+++ src/main/java/com/utfpr/rna/perceptron/MultiLayerPerceptron.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.utfpr.rna.perceptron;
+
+/**
+ *
+ * @author Marlon Prudente
+ */
+public class MultiLayerPerceptron {
+    public MultiLayerPerceptron(){
+        
+    }
+    
+}
