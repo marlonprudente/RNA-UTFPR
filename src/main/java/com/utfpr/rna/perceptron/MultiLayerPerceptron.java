703cf62afc9b340cdc8945dbefd7f19756d97ac5
Marlon Prudente <marlonoliveira@alunos.utfpr.edu.br>
2018-04-19 18:28:19 -0300

    MultiLayerPerceptron para XOR

--- src/main/java/com/utfpr/rna/perceptron/MultiLayerPerceptron.java
+++ src/main/java/com/utfpr/rna/perceptron/MultiLayerPerceptron.java
@@ -18,11 +18,11 @@ public class MultiLayerPerceptron {
 
     private Double taxa_aprendizado;
     private Double x0;
-    private int epoca;
+    private int epocas;
     private int w;
-    private Double[] pesos;    
-    private Double[][] treinamento_xor = {{0.0, 0.0, 0.0},{0.0, 1.0, 1.0}, {1.0, 0.0, 1.0}, {1.0,1.0,0.0}};    
-    
+    private Double[] pesos;
+    private Double[][] treinamento_xor = {{0.0, 0.0, 0.0}, {0.0, 1.0, 1.0}, {1.0, 0.0, 1.0}, {1.0, 1.0, 0.0}};
+
     public static Double Tanh(Double x) {
         return tanh(x);
     }
@@ -49,27 +49,72 @@ public class MultiLayerPerceptron {
         saida += pesos[2] * entrada2;
         return saida;
     }
-    
-    
-    public MultiLayerPerceptron(Double taxa_aprendizado, Integer epocas, Boolean isBatelada) {
+
+    public MultiLayerPerceptron(Double aprendizado, Integer epocas, Boolean isBatelada) {
         this.x0 = 1.0;
         w = 9;
-        this.taxa_aprendizado = taxa_aprendizado;        
-        this.epoca = epocas; 
+        this.taxa_aprendizado = aprendizado;
+        this.epocas = epocas;
         pesos = new Double[w];
-        
+
         Random r = new Random();
         for (int i = 0; i < w; i++) {
-            pesos[i] = r.nextDouble();            
+            pesos[i] = r.nextDouble();
         }
-        
-        try{
+
+        try {
             FileWriter arq = new FileWriter("logs/saida_mlp.txt");
             PrintWriter gravarArq = new PrintWriter(arq);
-            
-        }catch(Exception e){
-            
+            Double[] Y = new Double[2];
+            Double[] dwsaida = new Double[3];
+            Double[][] dwY = new Double[2][3];
+            Double saida;
+            Double erro;
+            Double ebp;
+            for (int epoca = 0; epoca < this.epocas; epoca++) {
+
+                for (int tam = 0; tam < 4; tam++) {
+                    Y[0] = Tanh(potencialAtivacao(treinamento_xor[tam][0], treinamento_xor[tam][1], pesos, "N1"));
+                    Y[1] = Tanh(potencialAtivacao(treinamento_xor[tam][0], treinamento_xor[tam][1], pesos, "N2"));
+                    saida = Tanh(potencialAtivacao(Y[0], Y[1], pesos, "NS"));
+                    erro = treinamento_xor[tam][2] - saida;
+                    if(erro > 0.01){
+                        //delta
+                        ebp = (erro*DerivadaTanh(potencialAtivacao(Y[0], Y[1], pesos, "NS")));
+                        //deltaW Neuronio Saída
+                        dwsaida[0] = taxa_aprendizado*ebp*x0;
+                        dwsaida[1] = taxa_aprendizado*ebp*Y[0];
+                        dwsaida[2] = taxa_aprendizado*ebp*Y[1];                        
+                        //deltaW Neuronio 1
+                        dwY[0][0] =  taxa_aprendizado*ebp*pesos[7]*DerivadaTanh(potencialAtivacao(treinamento_xor[tam][0], treinamento_xor[tam][1], pesos, "N1"))*x0;
+                        dwY[0][1] =  taxa_aprendizado*ebp*pesos[7]*DerivadaTanh(potencialAtivacao(treinamento_xor[tam][0], treinamento_xor[tam][1], pesos, "N1"))*treinamento_xor[tam][0];
+                        dwY[0][2] =  taxa_aprendizado*ebp*pesos[7]*DerivadaTanh(potencialAtivacao(treinamento_xor[tam][0], treinamento_xor[tam][1], pesos, "N1"))*treinamento_xor[tam][1];                        
+                        //deltaW Neuronio 2
+                        dwY[1][0] =  taxa_aprendizado*ebp*pesos[8]*DerivadaTanh(potencialAtivacao(treinamento_xor[tam][0], treinamento_xor[tam][1], pesos, "N1"))*x0;
+                        dwY[1][1] =  taxa_aprendizado*ebp*pesos[8]*DerivadaTanh(potencialAtivacao(treinamento_xor[tam][0], treinamento_xor[tam][1], pesos, "N1"))*treinamento_xor[tam][0];
+                        dwY[1][2] =  taxa_aprendizado*ebp*pesos[8]*DerivadaTanh(potencialAtivacao(treinamento_xor[tam][0], treinamento_xor[tam][1], pesos, "N1"))*treinamento_xor[tam][1];                       
+                        
+                        //Atualização pesos Neuronio 1
+                        pesos[0] += dwY[0][0];
+                        pesos[1] += dwY[0][1];
+                        pesos[2] += dwY[0][2];
+                        //Atualização pesos Neuronio 2
+                        pesos[3] += dwY[1][0];
+                        pesos[4] += dwY[1][1];
+                        pesos[5] += dwY[1][2];
+                        //Atualização pesos Neuronio Saída
+                        pesos[6] += dwsaida[0];
+                        pesos[7] += dwsaida[1];
+                        pesos[8] += dwsaida[2];
+                    }
+                }
+            }
+
+        } catch (Exception e) {
+            System.out.println("Err: " + e);
         }
+        
+        
     }
 
 }
